63541bafade9384e7f5b5c79110f7c402326686f
zarrar79 <shahzarrar79@gmail.com>
2024-01-25 16:45:56 +0500

    card responsive

--- src/Components/GPT_3.js
+++ src/Components/GPT_3.js
@@ -10,7 +10,7 @@ export const GPT_3 = () => {
                     {/* Card Parent */}
                     <div className='card_bg p-16 pt-0 pb-9 mt-16'>
                         {/* What is gpt-3 */}
-                        <div className='grid grid-cols-2 pt-14'>
+                        <div className='what grid grid-cols-4 pt-14 gap-10'>
                             {/* bar+head */}
                             <div>
                                 {/* bar */}
@@ -19,7 +19,7 @@ export const GPT_3 = () => {
                                 <div className='font-Manrope text-[24px] font-[800] text-[#FFFFFF] mt-4'>What is GPT-3</div>
                             </div>
                             {/* text */}
-                            <div className='font-Manrope text-[#81AFDD] text-[16px] text-[500] mt-[1.35rem]'>
+                            <div className='font-Manrope text-[#81AFDD] text-[16px] col-span-3 text-[500] mt-[1.35rem]'>
                                 We so opinion friends me message as delight. Whole front do of plate
                                 heard oh ought.
                                 His defective nor convinced residence own. Connection has put impossible
@@ -28,25 +28,25 @@ export const GPT_3 = () => {
                             </div>
                         </div>
                         {/* Possiblity+libray */}
-                        <div className='flex justify-between mt-[5.5rem]'>
+                        <div className='poss_lib flex justify-between mt-[5.5rem]'>
                             {/* Possibility */}
-                            <div className='font-Manrope text-[34px] font-[800] custom-text-gradient'>The possibilities are beyond<br></br> your imagination</div>
+                            <div className='poss font-Manrope text-[34px] font-[800] custom-text-gradient'>The possibilities are beyond<br></br> your imagination</div>
                             {/* library */}
-                            <div className='text-center pt-2 font-Manrope text-[500] text-[16px] text-[#FF8A71]'>Explore The Library</div>
+                            <div className='library text-center pt-2 font-Manrope text-[500] text-[16px] text-[#FF8A71]'>Explore The Library</div>
                         </div>
                         {/* Parent of boxes */}
-                        <div className='grid grid-cols-3 gap-10 mt-[5.5rem]'>
+                        <div className='parent_boxes grid grid-cols-3 gap-10 mt-[5.5rem]'>
                             {/* chatbot */}
                             <div>
                                 {/* bar+chat */}
                                 <div>
                                     {/* bar */}
-                                    <div className='max-w-[38px] h-[3px] element_2'></div>
+                                    <div className='bar max-w-[38px] h-[3px] element_2'></div>
                                     {/* chat */}
-                                    <div className='font-Manrope text-[24px] font-[800] text-[#FFFFFF] mt-4'>Chatbots</div>
+                                    <div className='chat font-Manrope text-[24px] font-[800] text-[#FFFFFF] mt-4'>Chatbots</div>
                                 </div>
                                 {/* text */}
-                                <div className='font-Manrope text-[#81AFDD] text-[16px] text-[500] mt-[1.35rem]'>
+                                <div className='text font-Manrope text-[#81AFDD] text-[16px] text-[500] mt-[1.35rem]'>
                                     We so opinion friends me message as delight. Whole front do of plate heard oh ought.
                                 </div>
                             </div>
@@ -57,10 +57,10 @@ export const GPT_3 = () => {
                                     {/* bar */}
                                     <div className='max-w-[38px] h-[3px] element_2'></div>
                                     {/* knowledge */}
-                                    <div className='font-Manrope text-[24px] font-[800] text-[#FFFFFF] mt-4'>Knowledgebase</div>
+                                    <div className='chat font-Manrope text-[24px] font-[800] text-[#FFFFFF] mt-4'>Knowledgebase</div>
                                 </div>
                                 {/* text */}
-                                <div className='font-Manrope text-[#81AFDD] text-[16px] text-[500] mt-[1.35rem]'>
+                                <div className='text font-Manrope text-[#81AFDD] text-[16px] text-[500] mt-[1.35rem]'>
                                     At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b
                                 </div>
                             </div>
@@ -71,10 +71,10 @@ export const GPT_3 = () => {
                                     {/* bar */}
                                     <div className='max-w-[38px] h-[3px] element_2'></div>
                                     {/* education */}
-                                    <div className='font-Manrope text-[24px] font-[800] text-[#FFFFFF] mt-4'>Education</div>
+                                    <div className='chat font-Manrope text-[24px] font-[800] text-[#FFFFFF] mt-4'>Education</div>
                                 </div>
                                 {/* text */}
-                                <div className='font-Manrope text-[#81AFDD] text-[16px] text-[500] mt-[1.35rem]'>
+                                <div className='text font-Manrope text-[#81AFDD] text-[16px] text-[500] mt-[1.35rem]'>
                                     At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b
                                 </div>
                             </div>
